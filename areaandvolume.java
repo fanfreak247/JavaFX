e95f0152a1a54d66fef7ff5094fd33a7245e38eb
Kea Webb <keandrewinston@hotmail.com>
2017-03-17 09:38:27 -0400

    Delete areaandvolume.java

--- areaandvolume.java
+++
@@ -1,9 +0,0 @@
-public class Prism {
-	public double Area(double side1, double side2) {
-		return side1 * side2;
-	}
-
-    public double Volume(double length, double width, double height) {
-    	return (length*width*height);
-    }
-}
